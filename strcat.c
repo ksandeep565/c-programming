cbd07e9bddcbf684e5c40d8feec61cdef1332a88
ksandeep565 <ksandeepsandeep126@gmail.com>
2026-02-28 20:29:00 +0530

    Add files via upload

---
+++ strcat.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s1[100]="k.sandeep";
+	char s2[100]="is a good boy";
+	strcat(s1,s2);
+	printf("\n concatention string %s",s1);
+	return 0;	
+}
