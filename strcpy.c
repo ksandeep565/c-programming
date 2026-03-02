9405944bede6594d488d25672914c47117b75b21
ksandeep565 <ksandeepsandeep126@gmail.com>
2026-03-02 21:08:47 +0530

    Add files via upload

---
+++ strcpy.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{ 
+      char s1[100];
+      char s2[100]="hello sandeep";
+      strcpy(s1,s2);
+      printf("\nThe copied string is :%s",s1);
+      return 0;
+	
+}
